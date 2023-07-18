e2b4e5330e05f678509d54258387286923d4666f
RossignolD <meirconnor@gmail.com>
2023-07-18 19:33:54 -0400

    big commit! got a lot of the jeps jackpot stuff done

--- public_html/js/jepsJackpotCalculator.js
+++ public_html/js/jepsJackpotCalculator.js
@@ -1,36 +1,47 @@
-
-// let regex=/'clue'\d\d/
-
-// console.log(matches)
-
 let jepsBoard = document.getElementsByClassName("clue");
-    console.log(jepsBoard);
 
-number= Math.floor(Math.random()*30);
+//   let jackpot = Math.floor(Math.random() * 30);
+let jackpot = 24;
+function getJackpotEl(number, clues)
+{
+  return clues[number];
+}
 
-function tableFlatten(box){
-    flatTable=[].concat.apply([], box);
-    return flatTable}
+let x=getJackpotEl(jackpot, jepsBoard);
+console.log(x);
 
-$(document).ready(function(){
-    flatClues=tableFlatten(jepsBoard);
-    console.log(flatClues);
+function getIDFromNumber(number) {
+  let el = getJackpotEl(number, jepsBoard);
+  let myID = el.id;
+  return myID;
+}
 
-    function getJackpotNumber(no, clues){
-        return flatClues[number]};
-    jackpotSquare=getJackpotNumber(flatClues[number], flatClues);
-    console.log(jackpotSquare);
-jackpotSquare.classList.add("jovialJepsWinner");
+$(document).on("click", function (event) {
+  let t = event.target.id;
+  let element = document.getElementById(t);
+  if (t == jackpot) {
+    document.getElementById(t).classList.add("jovialJepsWinner");
+    console.log("yay");
+  } else {
+    console.log("nay");
+    return null;
+  }
 })
 
-    $(document).click(function(e) {
-        if ($(e.target).hasClass("jovialJepsWinner")){
-        console.log(':)');
-        } 
-        else {
-          console.log('\:\(');
-          return null
-        }
-      });
-
-   
+document.addEventListener("click", function (e) {
+  let elementClass = e.target.className;
+  if (elementClass === "clue jovialJepsWinner") {
+    window.open("/public_html/jepsJackpot.html");
+  }
+})
+$(document).ready(function(){
+  let myID = getIDFromNumber(jackpot, jepsBoard);
+  let myEl = document.getElementById(myID);
+  console.log(myEl);
+  let myWinnerHTML = myEl.outerHTML;
+  //  let x = winner.outerHTML;
+  console.log(myWinnerHTML);
+  let myWinnerURL=myWinnerHTML.slice(63,-40);
+  console.log(myWinnerURL);
+  return myWinnerURL;
+});
