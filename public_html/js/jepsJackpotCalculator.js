358ab5bdd2df79552acb9254087925c3d6203e06
RossignolD <meirconnor@gmail.com>
2023-07-17 20:40:31 -0400

    paradise lost... but regained after linting. look forward to a Jeps Jackpot title card

--- public_html/js/jepsJackpotCalculator.js
+++ public_html/js/jepsJackpotCalculator.js
@@ -1,40 +1,32 @@
-number = Math.floor(Math.random()*30)
-
-$(document).ready(function(){
-    jepsBoard = document.getElementsByClassName('clue');
+let jepsBoard = document.getElementsByClassName('clue');
     console.log(jepsBoard);
-    return(jepsBoard)
 
-});
+number= Math.floor(Math.random()*30);
 
 function tableFlatten(box){
     flatTable=[].concat.apply([], box);
     return flatTable}
-
 
 $(document).ready(function(){
     flatClues=tableFlatten(jepsBoard);
     console.log(flatClues);
 
     function getJackpotNumber(no, clues){
-        return clues[no]};this
-    jackpotSquare=getJackpotNumber(number, flatClues);
-    console.log(jackpotSquare)
-jackpotSquare.classList.add("jovialJepsJackpot")
+        return flatClues[number]};
+    jackpotSquare=getJackpotNumber(flatClues[number], flatClues);
+    console.log(jackpotSquare);
+jackpotSquare.classList.add("jovialJepsJackpot");
 })
 
-$(document).click(function(event) {
-    var clue = $(event.target).text();
-    console.log(clue)
-    if(event.target.classList.contains("jovialJepsJackpot")==true){
-        $('.jovialJepsJackpotCover').addClass('jovialJepsJackpotUncover');
+    $(document).click(function(e) {
+        if ($(e.target).hasClass('jovialJepsJackpot')) {
+          console.log(':)');
+          $('.jovialJepsJackpotCover').addClass('jovialJepsJackpotUncover');
         $('.jovialJepsJackpotCover').removeClass('jovialJepsJackpotCover');
-    }
-    else {
-        return null
-            }
+        } else {
+          console.log('\:\(');
+          return null
         }
-    )
-
+      });
 
    
