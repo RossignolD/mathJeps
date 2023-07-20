e08246e61225acd8dda584976dfe9bbf59acf752
RossignolD <meirconnor@gmail.com>
2023-07-19 23:56:16 -0400

    finished typing jovial clues! yay! onward to jubilee

--- public_html/js/jepsCalculations.js
+++ public_html/js/jepsCalculations.js
@@ -1,13 +1,39 @@
 let jepsBoard = document.getElementsByClassName("clue");
 
-let jackpot = Math.floor(Math.random() * 30);
+// let jovialJackpot = Math.floor(Math.random() * 30);
+
+// function jubileeJackpots() {
+//   const myList = [];
+//   while (myList.length < 2) {
+//     myList.push(Math.floor(Math.random() * 30));
+//   }
+//   return myList;
+// }
+
+// let myJubileeJackpots = jubileeJackpots();
+
+// function isJovialJackpot(clicked_id) {
+//   if (clicked_id === jovialJackpot) {
+//     window.open("../public_html/jepsJackpot.html");
+//   } else {
+//     window.open(getURL(clicked_id));
+//   }
+// }
+
+// function getURL(clicked_id) {
+//   let myID = getIDFromNumber(clicked_id);
+//   let myEl = document.getElementById(clicked_id);
+//   let myWinnerHTML = myEl.outerHTML;
+//   console.log(myWinnerHTML);
+//   let myWinnerURL = myWinnerHTML.slice(63, -40);
+//   console.log(myWinnerURL);
+//   return myWinnerURL;
+// }
 
 const jovialClicks = new Array(30).fill(0);
 const fullArray = new Array(30).fill(1);
+const jubileeClicks = new Array(30).fill(0);
 
-window.onload = function() {
-  isArrayFullJovial();  //example function call.
-};
 function getJackpotEl(number, clues) {
   return clues[number];
 }
@@ -18,26 +44,23 @@ function getIDFromNumber(number) {
   return myID;
 }
 
-// $(document).on("click", function (event) {
-// let  t = event.target.id;
-//   let element = document.getElementById(t);
-//   if (t == jackpot) {
-//     document.getElementById(t).classList.add("jovialJepsWinner");
-//     console.log("yay");
-//   } else {
-//     console.log("nay");
-//     return null;
-//   }
-// });
-
-function updateListItem(ID) {
+function updateListItemJovial(ID) {
   jovialClicks.splice(ID, 1, 1);
   return jovialClicks;
 }
 
-function replyClick(clicked_id) {
-  alert(clicked_id);
-  updateListItem(clicked_id);
+function updateListItemJubilee(ID) {
+  jubileeClicks.splice(ID, 1, 1);
+  return jubileeClicks;
+}
+
+function replyClickJovial(clicked_id) {
+  updateListItemJovial(clicked_id);
+  return null;
+}
+
+function replyClickJubilee(clicked_id) {
+  updateListItemJubilee(clicked_id);
   return null;
 }
 
@@ -48,21 +71,44 @@ document.addEventListener("click", function (e) {
   }
 });
 
-function compareArraysJovial(a=jovialClicks, b=fullArray){
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index]);
-
+function compareArraysJovial(a = jovialClicks, b = fullArray) {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
 }
 
-function isArrayFullJovial(){
-  if (compareArraysJovial() === true){
-    btn= document.getElementById("moveToJepsJubilee")
-    return $(btn).show()
+function isArrayFullJovial() {
+  if (compareArraysJovial() === true) {
+    let btn = document.getElementById("moveToJepsJubilee");
+    $(btn).show();
+    return true;
+  } else {
+   let  btn = document.getElementById("moveToJepsJubilee");
+    $(btn).hide();
+    return false;
   }
-  else {
-   btn= document.getElementById("moveToJepsJubilee")
-   return $(btn).hide()
-  };
-};
+}
+
+function compareArraysJubilee(a = jubileeClicks, b = fullArray) {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+}
+
+function isArrayFullJubilee() {
+  if (compareArraysJubilee() === true) {
+    let btn = document.getElementById("moveToJepsJockey");
+    $(btn).show();
+    return true;
+  } else {
+    let btn = document.getElementById("moveToJepsJockey");
+    $(btn).hide();
+    return false;
+  }
+}
